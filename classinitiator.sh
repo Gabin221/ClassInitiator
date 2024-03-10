0f8ebcb079a10e1e8448f764f25746b7829f1ae3
Gabin221 <110061374+Gabin221@users.noreply.github.com>
2024-03-10 20:51:25 +0100

    Update classinitiator.sh

--- classinitiator.sh
+++ classinitiator.sh
@@ -10,3 +10,4 @@ do
     echo -e "#include \"$nom_fichier.h\"\n\nusing namespace std;\n\n$Nom_fichier::$Nom_fichier(){\n\n}\n\n$Nom_fichier::~$Nom_fichier(){\n\n}" > $nom_fichier".cpp"
     echo -e "#ifndef ${Nom_fichier}_h\n#define ${Nom_fichier}_h\n\nclass $Nom_fichier{\n\tprivate:\n\n\tprotected:\n\n\tpublic:\n\t\t$Nom_fichier();\n\t\t~$Nom_fichier();\n};\n#endif" > $nom_fichier".h"
 done
+ 
